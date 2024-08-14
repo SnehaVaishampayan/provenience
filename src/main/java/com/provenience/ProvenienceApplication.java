cc004185af9cdc2ef589db4172c727b2b79db1aa
sneha <sneha_gadre@yahoo.com>
2024-08-14 10:42:09 -0700

    Initializer files except .idea

---
+++ src/main/java/com/provenience/ProvenienceApplication.java
@@ -0,0 +1,13 @@
+package com.provenience;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvenienceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvenienceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/provenience/ProvenienceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.provenience;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvenienceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
