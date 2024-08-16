a88fb62344aa4a6a7eff235ef4abc0c844874a28
sneha <sneha_gadre@yahoo.com>
2024-08-15 20:47:28 -0700

    Adding post, update and delete apis for Project

--- src/main/java/com/provenience/project/ProjectController.java
+++ src/main/java/com/provenience/project/ProjectController.java
@@ -1,26 +1,54 @@
 package com.provenience.project;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.Optional;
 
 // Expects JSON
 @RestController
+@RequestMapping("/api/projects")
 public class ProjectController {
-
-//    @GetMapping("/home")
-//    String home() {
-//        return "Home page";
-//    }
 
     private final ProjectRepository projRepo;
 
     public ProjectController(ProjectRepository projRepo) {
         this.projRepo = projRepo;
     }
-    @GetMapping("/api/getAllProjects")
+    @GetMapping("")
     List<Project> getAllProjects() {
         return projRepo.getAllProjects();
+    }
+    @GetMapping("/{id}")
+    Project getProjectById(@PathVariable Integer id){
+        Optional<Project> project = projRepo.getProjectById(id);
+        if(project.isEmpty()) {
+            throw  new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+        return project.get();
+    }
+
+    // post/Create
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED) // return created status.. instead of 200.. its 201
+    void addProject(@RequestBody Project project) {
+        projRepo.addProject(project);
+    }
+
+    // update
+
+    @PutMapping("")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    void updateProject(@RequestBody Project project) {
+        projRepo.updateProject(project);
+    }
+
+    // delete
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable Integer id) {
+        projRepo.delete(id);
     }
 }
--- src/main/java/com/provenience/project/ProjectRepository.java
+++ src/main/java/com/provenience/project/ProjectRepository.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class ProjectRepository {
@@ -14,9 +15,27 @@ public class ProjectRepository {
     List<Project> getAllProjects() {
         return projects;
     }
+    Optional<Project> getProjectById(Integer id) {
+        return projects.stream().filter(project -> project.id() == id).findFirst();
+    }
+
     @PostConstruct
     private void init() {
         projects.add(new Project(1, "project1", LocalDate.now(), LocalDate.now(), ProjectStatus.COMPLETED ));
         projects.add(new Project(2, "project2", LocalDate.now(), LocalDate.now(), ProjectStatus.PROGRESS ));
+    }
+
+    void addProject(Project project) {
+        projects.add(project);
+    }
+    void updateProject(Project project) {
+        Optional<Project> oldProject = getProjectById(project.id());
+        if(oldProject.isPresent()) {
+            projects.set(projects.indexOf(oldProject.get()), project);
+        }
+    }
+
+    void delete(Integer id) {
+        projects.removeIf(project -> project.id().equals(id));
     }
 }
